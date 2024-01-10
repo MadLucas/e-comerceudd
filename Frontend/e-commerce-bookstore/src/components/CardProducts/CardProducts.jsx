1cd3f971532d5333ca16a62c6e867fb2ad74d49e
Milenaandrews <mileandrews@gmail.com>
2024-01-09 23:19:36 -0300

    paypal2

--- Frontend/e-commerce-bookstore/src/components/CardProducts/CardProducts.jsx
+++ Frontend/e-commerce-bookstore/src/components/CardProducts/CardProducts.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
+const CardProducts = ({product}) => {
 
-const CardProducts = ({ product }) => {
-  const { title, price, _id, thumbnailUrl, authors, categories } = product;
+  const {name, price, _id, image} = product;
 
   return (
     <div>
       <Link className="no-underline" to={`/products/${_id}`}>
         <div className="group relative">
           <div className="w-full min-h-80 bg-gray-300 aspect-w-1 rounded-md overflow-hidden group-hover:opacity-60 lg:aspect-none">
-            <img src={thumbnailUrl} alt={title} className="w-full h-60 object-center lg:w-full lg:h-60"  />
+            <img src={image} alt={name} className="w-full h-60 object-center lg:w-full lg:h-60"  />
           </div>
           <div className="mt-4 justify-between">
             <div>
-              <h3 className="text-sm text-gray-700">{title}</h3>
-              <p className="text-sm text-gray-700">Authors: {authors.join(', ')}</p>
-              <p className="text-sm text-gray-700">Categories: {categories.join(', ')}</p>
+              <h3 className="text-sm text-gray-700">{name}</h3>
             </div>
             <p className="text-sm text-gray-700 font-medium">
               ${price}
@@ -23,7 +21,7 @@ const CardProducts = ({ product }) => {
         </div>
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default CardProducts;
+export default CardProducts
